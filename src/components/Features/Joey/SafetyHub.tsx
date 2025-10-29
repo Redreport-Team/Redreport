b8a1f5d83eabf6843a3985f013d0c18317883659
Gaspar <glarrain@nd.edu>
2025-10-29 19:15:02 -0400

    Add Content for Feature @JoeySalatino

---
+++ src/components/Features/Joey/SafetyHub.tsx
@@ -0,0 +1,14 @@
+// File Created for Joey's safety tips and a resource hub
+import React from "react";
+
+// Function for all JS or TS functions they should handle the logic
+function SafetyHub() {
+  //HTML Code handling Visual Structure
+  return (
+    <>
+      <div className=""></div>
+    </>
+  );
+}
+
+export default SafetyHub;
